ccc5043bba9704833b9f8df7a4f58411d9c8b85f
James Merfeld <james.merfeld1@navy.mil>
2016-07-06 08:46:53 -0400

    Saving work.

--- problemSets.py
+++ problemSets.py
@@ -2,67 +2,58 @@ import myLibs
 import random
 
 def FindEulerianCycle2(adjList):
-    eCycle = ""
-    numEdges = 0
-    startNodes = []
-    for node in adjList:
-        startNodes.append(node)
-        numEdges += len(adjList[node])
-    print startNodes
-    while(True):
-        workingNodes = []
-        for node in startNodes:
-            workingNodes.append(node)
-        while len(workingNodes) > 0:
-            cycle = []
-            visited = []
-            currentNode = workingNodes[random.randint(0, len(workingNodes)-1)]
-            workingNodes.remove(currentNode)
-            while len(visited) < numEdges:
-                #print "Current node: " + str(currentNode)
-                #print "Visited edges: " + str(visited)
-                nextNodes = adjList[currentNode]
-                for node in nextNodes:
-                    key = str(currentNode)+str(node)
-                    if key in visited:
-                        nextNodes.remove(node)
-                if not nextNodes:
-                    print "Dead-end found"
-                    break
-                #print "Possible next nodes: " + str(nextNodes)
-                nextNode = nextNodes[random.randint(0, len(nextNodes)-1)]
-                #print "Next node: " + str(nextNode)
-                visited.append(str(currentNode)+str(nextNode))
-                currentNode = nextNode
-            if len(visited) == numEdges:
-                eCycle = str(visited)
-                break
-        if eCycle:
-            break
+    eCycle = []
+    nodes = dict(adjList)
+    currentNode = random.choice(adjList.keys())
+    while currentNode:
+        eCycle.append(currentNode)
+        print "Current node: " + str(currentNode)
+        nextNodes = adjList[currentNode]
+        print "Next nodes: " + str(nextNodes)
+        if not nextNodes:
+            print "Dead-end reached"
+            myLibs.PrintAdjList(nodes)
+            currentNode = ""
+        else:
+            index = random.randint(0,len(nextNodes)-1)
+            nextNode = nextNodes[index]
+            print "Next node: " + str(nextNode)
+            nodes[currentNode].remove(nextNode)
+            currentNode = nextNode
     return eCycle
 
 def FindEulerianCycle(adjList):
     eCycle = []
-    startNodes = []
-    edges = []
+    nodes = dict(adjList)
+    currentNode = random.choice(adjList.keys())
 
-    #Generate lists of start nodes and edges
-    for node in adjList:
-        startNodes.append(node)
-        for n in adjList[node]:
-            edges.append(str(node)+str(n))
-    #Get first start node
-    startNode = startNodes[random.randint(0, len(startNodes) - 1)]
-    toVisit = list(edges)
-    nextNodes = []
-    for node in adjList[startNode]:
-        key = str(startNode)+str(node)
-        if key in toVisit:
-            nextNodes.append(node)
-    
+    #form initial cycle
+    while True:
+        eCycle.append(currentNode)
+        print "Current node: " + str(currentNode)
+        nextNodes = adjList[currentNode]
+        print "Next nodes: " + str(nextNodes)
+        if not nextNodes:
+            print "Dead-end reached"
+            myLibs.PrintAdjList(nodes)
+            nextNodes = []
+            for node in eCycle:
+                if len(adjList[node]) > 0:
+                    nextNodes.append(node)
+            if not nextNodes:
+                print "Eulerian Cycle found: " + str(eCycle)
+                return eCycle;
+            else:
+                print "Next nodes: " + str(nextNodes)
+            break;
+        else:
+            index = random.randint(0,len(nextNodes)-1)
+            nextNode = nextNodes[index]
+            print "Next node: " + str(nextNode)
+            nodes[currentNode].remove(nextNode)
+            currentNode = nextNode
 
-
-
+    #pick newStart from Cycle
     return eCycle
 
 #Week2, 1-2, Step2 - Code Challenge
@@ -89,5 +80,4 @@ for line in contents:
 
 fOut.close()
 
-print(FindEulerianCycle(adjList))
-#myLibs.PrintAdjList(adjList)
+FindEulerianCycle(adjList)
